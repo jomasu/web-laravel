d47500c5bde81b48a1abea0f1ce7df3f4784be81
jomasu <jmarrujos@gmail.com>
2018-11-07 20:15:46 -0300

    primer push

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,80 @@
+@section('footer')
+<footer id="footer"><!--Footer-->
+		
+		
+		<div class="footer-widget">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Categorías</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Celulares</a></li>
+								<li><a href="#">Notebooks</a></li>
+                                <li><a href="#">Smart TV</a></li>
+                                <li><a href="#">Accesorios</a></li>
+								<li><a href="#">Todas las categorías</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Marcas</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Asus</a></li>
+								<li><a href="#">Samsung</a></li>
+								<li><a href="#">LG</a></li>
+								<li><a href="#">Sony</a></li>
+								<li><a href="#">Todas las marcas</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Servicios</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Tus compras</a></li>
+								<li><a href="#">Ayuda en línea</a></li>
+								<li><a href="#">He tenido un problema</a></li>
+								<li><a href="#">Contáctanos</a></li>
+								<li><a href="#">Ticket System</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Sobre nosotros</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">¿Quienes Somos?</a></li>
+								<li><a href="#">Puntos de entrega</a></li>
+								<li><a href="#">Politicas de privacidad</a></li>
+                                <li><a href="#">Términos y condiciones</a></li>
+                                <li><a href="#">Trabajá con nosotros</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3 col-sm-offset-1">
+						<div class="single-widget">
+							<h2>Registrate</h2>
+							<form action="#" class="searchform">
+								<input type="text" placeholder="Tu email" />
+								<button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
+								<p>Recibe a tu casilla de correo <br />las ofertas más destacadas..</p>
+							</form>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Tfire SA. Todos los derechos reservados.</p>
+				</div>
+			</div>
+		</div>
+		
+	</footer><!--/Footer-->
+	@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>App Name - @yield('title')</title> 
+
+    <!-- code CSS-->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/prettyPhoto.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/price-range.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
+
+    
+    
+</head>
+<body>
+@section('header')
+<header id="header"><!--header-->
+		<div class="header_top"><!--header_top-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="contactinfo">
+							<ul class="nav nav-pills">
+								<li><a href="#"><i class="fa fa-phone"></i> 0800 333 2425</a></li>
+								<li><a href="#"><i class="fa fa-envelope"></i> info@tfire.com</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="social-icons pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header_top-->
+		
+		<div class="header-middle"><!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 clearfix">
+						<div class="logo pull-left">
+							<a href="index.html"><img src="images/home/tfire.png" alt="" /></a>
+						</div>
+					</div>
+					<div class="col-md-8 clearfix">
+						<div class="shop-menu clearfix pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href=""><i class="fa fa-user"></i> Nosotros</a></li>
+								<li><a href=""><i class="fa fa-star"></i> Mi lista</a></li>
+								<li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Salir</a></li>
+								<li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Carrito</a></li>
+								<li><a href="login.html"><i class="fa fa-lock"></i> Ingresar</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-middle-->
+	
+		<div class="header-bottom"><!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="index.html" class="active">Home</a></li>
+								<li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="shop.html">Productos</a></li>
+										<li><a href="product-details.html">Product Details</a></li> 
+										<li><a href="checkout.html">Checkout</a></li> 
+										<li><a href="cart.html">Cart</a></li> 
+										<li><a href="login.html">Login</a></li> 
+                                    </ul>
+                                </li> 
+								<li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="blog.html">Blog List</a></li>
+										<li><a href="blog-single.html">Blog Single</a></li>
+                                    </ul>
+                                </li> 
+								<li><a href="404.html">404</a></li>
+								<li><a href="contact-us.html">Sucursales</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="search_box pull-right">
+							<input type="text" placeholder="Buscar"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-bottom-->
+    </header><!--/header-->
+
+    @show
+
+    <div class="container">
+            @yield('content')
+    </div>
+
+    <div>
+        @yield('body')
+    </div>
+
+    <div>
+        @yield('footer')
+    </div>    
+    
+
+
+<!-- code javascript-->
+<script src="{{ asset('js/jquery.js') }}" defer></script>
+<script src="{{ asset('js/bootstrap.min.js') }}" defer></script>
+<script src="{{ asset('js/jquery.scrollUp.min.js') }}" defer></script>
+<script src="{{ asset('js/price-range.js') }}" defer></script>
+<script src="{{ asset('js/jquery.prettyPhoto.js') }}" defer></script>
+<script src="{{ asset('js/main.js') }}" defer></script>
+
+
+</body>
+</html>
