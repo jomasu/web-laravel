e05135cca703c14f50f4808e8ee416c2858f9c69
grampinini <gianfranco_rampinini@hotmail.com>
2018-11-23 20:54:16 -0300

    homeArreglado

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -88,4 +88,50 @@ class InicioController extends Controller
     {
         //
     }
+
+    public function imagesUpload()
+
+    {
+
+        return view('imagesUpload');
+
+    }
+
+
+
+    /**
+
+     * Create a new controller instance.
+
+     *
+
+     * @return void
+
+     */
+
+    public function imagesUploadPost(Request $request)
+
+    {
+
+        request()->validate([
+
+            'uploadFile' => 'required',
+
+        ]);
+
+
+
+        foreach ($request->file('uploadFile') as $key => $value) {
+
+            $imageName = time(). $key . '.' . $value->getClientOriginalExtension();
+
+            $value->move(public_path('img'), $imageName);
+
+        }
+
+
+
+        return response()->json(['success'=>'Las imagenes se cargaron correctamente.']);
+	
+    }
 }
