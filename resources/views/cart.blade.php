d104dfa5c01cb93f054a665083145d4fffcad66b
jomasu <jmarrujos@gmail.com>
2018-11-27 23:53:06 -0300

    vista carrito lista

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -8,19 +8,21 @@ Incorporar a la vista -->
 
 
 @section('body')
-<section id="cart_items">
-		<div class="container">
-			<div class="breadcrumbs">
-				<ol class="breadcrumb">
-				  <li><a href="/">Home</a></li>
-				  <li class="active">Shopping Cart</li>
-				</ol>
-            </div>
+<div class="container">
+				<div class="breadcrumbs">
+					<ol class="breadcrumb">
+					<li><a href="/">Home</a></li>
+					<li class="active">Shopping Cart</li>
+					</ol>
+				</div>
+	<section class="col-md-9" id="cart_items">
+		
+				
             @empty($products) 
             <h1>No hay productos en el carrito</h1>
             <br><br><br><br>
             @else
-			<div class="table-responsive cart_info">
+			<div class=" table-responsive cart_info">
 				<table class="table table-condensed">
 					<thead>
 						<tr class="cart_menu">
@@ -55,7 +57,7 @@ Incorporar a la vista -->
 								</div>
 							</td>
 							<td class="cart_total">
-								<p class="cart_total_price">{{number_format($product->price * )}}</p>
+								<p class="cart_total_price">{{number_format($product->price)}}</p>
 							</td>
 							<td class="cart_delete">
 								<a class="{{ route('cart.remove', $product->id) }}" href=""><i class="fa fa-times"></i></a>
@@ -67,86 +69,32 @@ Incorporar a la vista -->
 					</tbody>
 				</table>
 			</div>
-		</div>
+		
 	</section> <!--/#cart_items-->
 
-	<section id="do_action">
-		<div class="container">
-			<div class="heading">
-				<h3>What would you like to do next?</h3>
-				<p>Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.</p>
-			</div>
-			<div class="row">
-				<div class="col-sm-6">
-					<div class="chose_area">
-						<ul class="user_option">
-							<li>
-								<input type="checkbox">
-								<label>Use Coupon Code</label>
-							</li>
-							<li>
-								<input type="checkbox">
-								<label>Use Gift Voucher</label>
-							</li>
-							<li>
-								<input type="checkbox">
-								<label>Estimate Shipping & Taxes</label>
-							</li>
-						</ul>
-						<ul class="user_info">
-							<li class="single_field">
-								<label>Country:</label>
-								<select>
-									<option>United States</option>
-									<option>Bangladesh</option>
-									<option>UK</option>
-									<option>India</option>
-									<option>Pakistan</option>
-									<option>Ucrane</option>
-									<option>Canada</option>
-									<option>Dubai</option>
-								</select>
-								
-							</li>
-							<li class="single_field">
-								<label>Region / State:</label>
-								<select>
-									<option>Select</option>
-									<option>Dhaka</option>
-									<option>London</option>
-									<option>Dillih</option>
-									<option>Lahore</option>
-									<option>Alaska</option>
-									<option>Canada</option>
-									<option>Dubai</option>
-								</select>
+	<section class="col-sm-3" id="do_action">
+		
+			
+			<div class="row total_area">
+				
+				
+					<div class=" padding-cart">
+						
 							
-							</li>
-							<li class="single_field zip-field">
-								<label>Zip Code:</label>
-								<input type="text">
-							</li>
-						</ul>
-						<a class="btn btn-default update" href="">Get Quotes</a>
-						<a class="btn btn-default check_out" href="">Continue</a>
+							<p>Sub Total <span>$100.00</span></p>
+							<p>Precio de Envio <span>Free</span></p>
+							<hr>
+							<p>Total <span>$1000.00</span></p>
+						
+							<a class="btn btn-default btn-primary btn-block" href="">Borrar todo</a>
+							<a class="btn btn-default btn-primary btn-block" href="">Finalizar Compra</a>
 					</div>
-				</div>
-				<div class="col-sm-6">
-					<div class="total_area">
-						<ul>
-							<li>Cart Sub Total <span>$59</span></li>
-							<li>Eco Tax <span>$2</span></li>
-							<li>Shipping Cost <span>Free</span></li>
-							<li>Total <span>$61</span></li>
-						</ul>
-							<a class="btn btn-default update" href="">Update</a>
-							<a class="btn btn-default check_out" href="">Check Out</a>
-					</div>
-                </div>
+                
                 @endempty
 			</div>
-		</div>
-    </section><!--/#do_action-->
+		
+	</section><!--/#do_action-->
+</div>	
     @endsection
     <!--Footer de la página-->
 @section('footer')
